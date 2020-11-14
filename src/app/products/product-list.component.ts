3843c4d7748d24e068c10031aadd561d712bdafd
mpys_sii <mpys@sii.pl>
2020-11-14 13:51:13 +0100

    Typo fix

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
     ]
 })
 export class ProductListComponent {
-    pageTitle: string = 'Product List';
+    pageTitle: string = 'Products List';
     products: any[] = [
         {
             "productId": 1,
